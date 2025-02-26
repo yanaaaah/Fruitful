63d307b121062134120ad55d8cea05c3c57d624d
Tilly <tenicoll02@gmail.com>
2025-02-26 11:45:28 +1100

    this is the logic for logging view - i used an evironemnt

---
+++ Fruitfull/DaySet.swift
@@ -0,0 +1,37 @@
+//
+//  Structdayset.swift
+//  Fruitfull
+//
+//  Created by Tilly Nicoll on 25/2/2025.
+//
+
+//import SwiftUI
+//
+//// Data needed for TeamMembersListView and TeamMemberView
+//
+//struct Structdayset: Identifiable {
+//    var body: some View {
+//            let slep = UUID()
+//            var movement: String
+//            var profilePicture: String
+//            var personalStrength: String
+//            var pronouns: String
+//            var appleID: String
+//        }
+//
+//        let members: [Structdayset] = [
+//            Structdayset(
+//                name: "Tony Tiger",
+//                profilePicture: "tonyTigerPicture",
+//                personalStrength: "courage"
+//                pronouns: "he/him"
+//                appleID: "tony.tiger@icloud.com"
+//            )
+//            // Add all the other TeamMembers here
+//        ]
+//
+//}
+//
+//#Preview {
+//    Structdayset()
+//}
---
+++ Structdayset.swift
@@ -0,0 +1,18 @@
+//
+//  Structdayset.swift
+//  Fruitfull
+//
+//  Created by Tilly Nicoll on 25/2/2025.
+//
+
+import SwiftUI
+
+struct Structdayset: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Structdayset()
+}
