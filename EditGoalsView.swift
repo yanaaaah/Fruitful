528d55acc814f7697922592ccfdaa234931e4f6a
samurai <f2tpndk27y@privaterelay.appleid.com>
2025-02-26 11:48:11 +1100

    AccountViewFinal

---
+++ EditGoalsView.swift
@@ -0,0 +1,18 @@
+//
+//  EditGoalsView.swift
+//  Fruitfull
+//
+//  Created by Abhijeet Raj Singh on 26/2/2025.
+//
+
+import SwiftUI
+
+struct EditGoalsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditGoalsView()
+}
---
+++ Fruitfull/ICloudSyncView.swift
@@ -0,0 +1,18 @@
+//
+//  ICloudSyncView.swift
+//  Fruitfull
+//
+//  Created by Abhijeet Raj Singh on 26/2/2025.
+//
+
+import SwiftUI
+
+struct ICloudSyncView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ICloudSyncView()
+}
